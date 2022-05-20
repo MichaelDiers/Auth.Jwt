8da927bc4dcf996d76020106dd81592131935423
MichaelDiers <md@sreid.de>
2022-05-20 21:04:19 +0200

    string extensions

--- Auth.Jwt.Web/Extensions/StringExtensions.cs
+++ Auth.Jwt.Web/Extensions/StringExtensions.cs
@@ -6,6 +6,16 @@
     public static class StringExtensions
     {
         /// <summary>
+        ///     Remove the controller suffix from a controller name.
+        /// </summary>
+        /// <param name="controller">The of the controller.</param>
+        /// <returns>The name of the controller without its suffix.</returns>
+        public static string ControllerName(this string controller)
+        {
+            return controller.Substring(0, controller.Length - 10);
+        }
+
+        /// <summary>
         ///     Normalizes a user name for database operations.
         /// </summary>
         /// <param name="userName">The name of the user.</param>
