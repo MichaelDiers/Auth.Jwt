ab2bd9fb1dae260a99b91f759d5af1c46cca2b9f
MichaelDiers <md@sreid.de>
2022-05-21 22:50:49 +0200

    validation attribute

---
+++ Auth.Jwt.Web/Attributes/PasswordStringLength.cs
@@ -0,0 +1,23 @@
+﻿namespace Auth.Jwt.Web.Attributes
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class PasswordStringLength : StringLengthAttribute
+    {
+        public const string PasswordErrorMessage = "StringLength";
+        public const int PasswordMaximumLength = 100;
+
+        public const int PasswordMinimumLength = 4;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="PasswordStringLength" />
+        ///     class.
+        /// </summary>
+        public PasswordStringLength()
+            : base(PasswordStringLength.PasswordMaximumLength)
+        {
+            this.MinimumLength = PasswordStringLength.PasswordMinimumLength;
+            this.ErrorMessage = PasswordStringLength.PasswordErrorMessage;
+        }
+    }
+}
